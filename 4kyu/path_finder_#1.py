7dad1de01340bad6eeedb24dc81b21cf445a16c4
zizou <zizouvb@users.noreply.github.com>
2020-06-01 10:28:20 +0200

    Update path_finder_#1.py

--- 4kyu/path_finder_#1.py
+++ 4kyu/path_finder_#1.py
@@ -18,17 +18,14 @@ def get_free_neighbors(maze,node,N):
     
 def path_finder(maze_str):
     maze=[list(line) for line in maze_str.split("\n")]
-    print(maze)
     N=len(maze)
-    node = [0,0]
+    node = [N-1,N-1]
     queue = [node]
     while queue:
         node = queue.pop(0)
         y,x = node
-        if x==N-1 and y==N-1:
+        if x==0 and y==0:
             return True
         else:
             maze[y][x]="x"
-        queue.extend(get_free_neighbors(maze,node,N))
-        
-    return False
+        queue=get_free_neighbors(maze,node,N)+queue
