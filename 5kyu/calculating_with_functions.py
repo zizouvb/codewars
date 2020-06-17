7fff55d523973148ed43d45560d1d82293e9053d
zizou <zizouvb@users.noreply.github.com>
2020-06-17 08:51:19 +0200

    Create calculating_with_functions.py

---
+++ 5kyu/calculating_with_functions.py
@@ -0,0 +1,40 @@
+def zero(i=""): return eval("0" + i)
+
+
+def one(i=""): return eval("1" + i)
+
+
+def two(i=""): return eval("2" + i)
+
+
+def three(i=""): return eval("3" + i)
+
+
+def four(i=""): return eval("4" + i)
+
+
+def five(i=""): return eval("5" + i)
+
+
+def six(i=""): return eval("6" + i)
+
+
+def seven(i=""): return eval("7" + i)
+
+
+def eight(i=""): return eval("8" + i)
+
+
+def nine(i=""): return eval("9" + i)
+
+
+def plus(i): return '+' + str(i)
+
+
+def minus(i): return '-' + str(i)
+
+
+def times(i): return '*' + str(i)
+
+
+def divided_by(i): return '//' + str(i)
